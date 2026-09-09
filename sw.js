d4d120e38befa71343fcdda81fddc4feb6d58796
hyperproductionsa <hyperproductionsa@gmail.com>
2026-09-09 09:05:39 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'hts-player-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/player.js',
+  '/data.json',
+  '/manifest.json',
+  '/images/icon-192x192.png',
+  '/images/icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS_TO_CACHE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
